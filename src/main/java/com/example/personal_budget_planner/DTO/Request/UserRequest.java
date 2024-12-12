5242008681097c7dbf930cad5f1320daabf46d8e
bharathrajb6 <bharathrajb26@gmail.com>
2024-12-12 20:46:28 +0530

    Added models

---
+++ src/main/java/com/example/personal_budget_planner/DTO/Request/UserRequest.java
@@ -0,0 +1,4 @@
+package com.example.personal_budget_planner.DTO.Request;
+
+public class UserRequest {
+}
---
+++ src/main/java/com/example/personal_budget_planner/Model/Transaction.java
@@ -0,0 +1,4 @@
+package com.example.personal_budget_planner.Model;
+
+public class Transaction {
+}
