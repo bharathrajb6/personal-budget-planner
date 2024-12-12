6c973fbb07388dd5e19a636c4b3c79cd3811d352
bharathrajb6 <bharathrajb26@gmail.com>
2024-12-12 20:20:37 +0530

    Initial Commit of project files

---
+++ src/test/java/com/example/personal_budget_planner/PersonalBudgetPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personal_budget_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBudgetPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
